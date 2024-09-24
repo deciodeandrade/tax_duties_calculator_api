367924c4247f11e1cfea9abf47c0edbbda6155e8
Décio <deciocavalcantedeandrade@gmail.com>
2024-09-24 10:35:52 -0300

    Correção de rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :tax_calculator do
-    post 'tax-calculation', to: 'tax_calculation#calculate'
-    post 'tax-scenario-simulation', to: 'tax_scenario_simulation#simulate'
-    post 'source-withholding', to: 'source_withholding#calculate'
+    post 'tax_calculation', to: 'tax_calculation#calculate'
+    post 'tax_scenario_simulation', to: 'tax_scenario_simulation#simulate'
+    post 'source_withholding', to: 'source_withholding#calculate'
   end
 end
